78a1bd618e92b918dd3875c5a6c887847749b95e
Dmitry Remesov <dmitry@remesov.ru>
2013-12-17 12:09:22 +0400

    Remove defines that are not needed and produce warnings

--- chmlib/chm_lib.c
+++ chmlib/chm_lib.c
@@ -188,9 +188,6 @@ typedef unsigned long           UInt64;
 
 /* GCC */
 #ifdef __GNUC__
-#define memcmp __builtin_memcmp
-#define memcpy __builtin_memcpy
-#define strlen __builtin_strlen
 
 #elif defined(WIN32)
 static int ffs(unsigned int val)
